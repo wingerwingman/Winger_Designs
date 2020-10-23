eabf8e9ca9af984c75c5a7becf6a070d536590c4
Justin Winger <justinwinger4@gmail.com>
2020-10-23 14:45:09 -0600

    new setup

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM node:10
-# where our app will be located in the image
-RUN mkdir -p /app
-WORKDIR /app
-# move all source code
-COPY . .
-RUN npm install
-CMD [ "npm", "run", "eb:prod" ]
-EXPOSE 3000
---
+++ app.js
@@ -0,0 +1,12 @@
+
+const express = require('express')
+const path = require('path')
+const port = process.env.PORT || 3000
+const app = express()
+
+app.use(express.static('./public'))
+
+app.get('/', (req, res, next) => {
+  res.send(path.join(__dirname + '/index.html'))
+}).listen(port)
+console.log('Server running at http://127.0.0.1:' + port + '/')
