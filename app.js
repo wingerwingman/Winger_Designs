b54aebe84a9ebd9f982e0e8e5c6ed2857b4acc40
Justin Winger <justinwinger4@gmail.com>
2020-10-23 14:48:43 -0600

    new setup

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ const app = express()
 app.use(express.static('./public'))
 
 app.get('/', (req, res, next) => {
-  res.send(path.join(__dirname + '/public/index.html'))
+  res.send(path.join(__dirname + './public/index.html'))
 }).listen(port)
 console.log('Server running at http://127.0.0.1:' + port + '/')
