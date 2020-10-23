be12180fe25520cdc820e46393238c2a92efbbae
Justin Winger <justinwinger4@gmail.com>
2020-10-23 11:55:22 -0600

    .ebext

---
+++ build/precache-manifest.99851f5c67477fe823ba74df17bb5562.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a136e935e615c88c5d127b0154d4645",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bfa6038864c1f8094c70",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "435c6c497413defbebd2",
+    "url": "/static/js/2.4026fcc8.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.4026fcc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfa6038864c1f8094c70",
+    "url": "/static/js/main.23169dfd.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
