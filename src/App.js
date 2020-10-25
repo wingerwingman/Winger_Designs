f7f2755699c7050ad7753de1de4be7aee0db1f8b
Justin Winger <justinwinger4@gmail.com>
2020-10-25 12:05:00 -0600

    Created Navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import NavBar from './components/NavBar'
+import Welcome from './components/Welcome'
+import Footer from './components/Footer'
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        Winger Designs
-      </header>
+      <NavBar />
+      <Route exact path="/" component={Welcome} />
+      <Footer />
     </div>
+    </Router>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => (
+
+    <div className="footer">
+      <small>&copy; Copyright 2020. All Rights Reserved</small>
+    </div>
+    
+);
+  
+export default Footer;
