dc3d4065ad5f83f41246a8b94a8cf143b686d2b1
Justin Winger <justinwinger4@gmail.com>
2020-10-26 14:58:48 -0600

    form styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -15,41 +15,45 @@ export default class Contact extends Component {
     render() {
         return (
             <div>
+                
                 <form id="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
+                    <fieldset>
                     <div className="form-group">
-                        <label htmlFor="name">Name</label>
+                        <label value={this.state.name} htmlFor="name">Name:<br/></label>
                         <input type="text" className="form-control" />
                     </div>
                     <div className="form-group">
-                        <label htmlFor="exampleInputEmail1">Email address</label>
+                        <label value={this.state.email}htmlFor="exampleInputEmail1">Email address:<br/></label>
                         <input type="email" className="form-control" aria-describedby="emailHelp" />
                     </div>
                     <div className="form-group">
-                        <label htmlFor="message">Message</label>
+                        <label value={this.state.message} htmlFor="message">Message:<br/></label>
                         <textarea className="form-control" rows="5"></textarea>
                     </div>
                         <button type="submit" className="btn btn-primary">Submit</button>
+                    </fieldset>
                 </form>
+                
             </div>
         )
     }
 
     onNameChange(event) {
         this.setState({name: event.target.value})
-      }
+    }
     
-      onEmailChange(event) {
+    onEmailChange(event) {
         this.setState({email: event.target.value})
-      }
+    }
     
-      onMessageChange(event) {
+    onMessageChange(event) {
         this.setState({message: event.target.value})
-      }
+    }
     
-      handleSubmit(event) {
+    handleSubmit(event) {
           debugger;
         event.preventDefault();
         console.log(this.state);
-      }
+    }
 
 }
